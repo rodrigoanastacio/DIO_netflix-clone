8364e03fe78944c78f8f625d3326ff39cf6db44d
Rodrigo Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-10-16 15:51:52 -0300

    start projetc

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'js/main.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 9000,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(svg|png|jp?g)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'images',
+          publicPath: 'images',
+        },
+      },
+      {
+        test: /.(scss|css)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: false,
+      hash: true,
+      inject: false,
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+  ],
+};
