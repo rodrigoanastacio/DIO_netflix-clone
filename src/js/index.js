c6b721c281eb127a5588bbd4ef2ad05430e3ca32
Rodrigo Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-11-04 19:02:14 -0300

    Feat - Desenvolvimento do carrossel de filmes

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
+import Carousel from "./components/Carousel";
+
 import "./../scss/style.scss";
+
 import img from "../images/capa-house.jpg";
+import thumb1 from "../images/mini1.jpg";
+import thumb2 from "../images/mini2.jpg";
+import thumb3 from "../images/mini3.jpg";
+import thumb4 from "../images/mini4.jpg";
+import thumb5 from "../images/mini5.jpg";
+import thumb6 from "../images/mini6.jpg";
+import thumb7 from "../images/mini7.jpg";
+import thumb8 from "../images/mini8.jpg";
+import thumb9 from "../images/mini9.jpg";
+import thumb10 from "../images/mini10.jpg";
